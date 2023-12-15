e1ae65a39a73d74ba4754cfb553d18fd64cba736
Franco Mendez <125481413+francomendezok@users.noreply.github.com>
2023-12-15 12:27:41 -0300

    Layout and Fonts States

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,19 @@
 import '../styles/index.css'
+import { useState } from 'react';
 import Resume from './Resume'
-import CreateResume from './CreateResume'
+import CreateResume from './CreateResume';
 
 
 
 export default function App() {
+  const [layout, updateLayout] = useState('resumeTop');
+  // const [color, updateColor] = useState('#000000');
+  const [fonts, updateFonts] = useState('sans-serif');
 
   return (
     <div className="container">
-      <CreateResume />
-      <Resume />
+      <CreateResume updateLayout={updateLayout} updateFonts={updateFonts}/>
+      <Resume layout={layout} fonts={fonts}/>
     </div>
   )
 }
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,13 +1,14 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 function ResumeInfo({ info }) {
     const { date, city, institution, position, description } = info;
     return (
-      <div className="flex items-start justify-evenly m-4">
-        <div>
+      <div className="resumeInfo h-full flex justify-around">
+        <div className="parragraph">
           <p>{date}</p>
           <p>{city}</p>
         </div>
-        <div>
+        <div className="w-2/3">
           <strong>{institution}</strong>
           <p>{position}</p>
           <p>{description}</p>
@@ -16,7 +17,7 @@ function ResumeInfo({ info }) {
     );
   }
 
-  function Skills({ skills }) {
+function Skills({ skills }) {
     return (
       <div className="flex items-center justify-center">
         {skills.map((skill, index) => (
@@ -26,35 +27,37 @@ function ResumeInfo({ info }) {
     );
   }
 
-
-export default function Resume () {
+  
+export default function Resume ({layout, fonts}) {
   const skillsIHave = ['Coding', 'Marketing'];
-
+  console.log(fonts);
     return (
-        <aside id="resume" className="bg-slate-200 shadow-xl">
+        <aside id={layout} style={{ fontFamily: fonts, fontSize: '1rem' }} className="bg-slate-200 shadow-xl">
             <div className="personalInfo bg-blue-300 p-6">
-                <h1 className="text-center text-4xl">Franco</h1>
+                <h1 className="text-center text-4xl mb-2">Franco</h1>
                 <div className="flex justify-around">
                     <span>Mail<img src="" alt="" /></span>
                     <span>Telephone<img src="" alt="" /></span>
                     <span>Adress<img src="" alt="" /></span>
                 </div>
             </div>
-            <div className="experienceBox">
+            <div className="infoContainer">
+              <div className="experienceBox">
                 <div className="cv-categories">Profesional Experience</div>
                 <ResumeInfo info={{ date: '10', city: 'City University', institution: 'University XYZ', position: "Bachelor's in Computer Science", description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' }} />
                 <ResumeInfo info={{ date: '10', city: 'City University', institution: 'University XYZ', position: "Bachelor's in Computer Science", description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' }} />
-            </div>
-            <div className="educationBox">
+              </div>
+              <div className="educationBox">
                 <div className="cv-categories">Education</div>
                 <ResumeInfo info={{ date: '10', city: 'City University', institution: 'University XYZ', position: "Bachelor's in Computer Science", description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' }} />
                 <ResumeInfo info={{ date: '10', city: 'City University', institution: 'University XYZ', position: "Bachelor's in Computer Science", description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' }} />
-            </div>
-            <div className="skillsBox">
+              </div>
+              <div className="skillsBox">
                 <div className="cv-categories">Skills</div>
                 <Skills skills={skillsIHave} />
-            </div>
+              </div>
+              </div>
         </aside>
     )
-}
+  }
 
