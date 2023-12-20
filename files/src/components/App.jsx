dc9026583637eb788b5555b6d7dccc238df3d46c
Franco Mendez <125481413+francomendezok@users.noreply.github.com>
2023-12-20 19:28:05 -0300

    Try to fix CompleteYourInfo State

--- files/src/components/App.jsx
+++ files/src/components/App.jsx
@@ -7,53 +7,12 @@ import CreateResume from './CreateResume';
 
 
 export default function App() {
-  const InfoExample = 
-  {
-    personal: {
-      fullname: 'Franco Nicolas Mendez',
-      email: 'francomendezok@gmail.com',
-      phone: '+5493513930405',
-      address: 'Cordoba, Argentina',
-    },
-    experience: [
-      {
-        company: 'Ama Hogar',
-        position: 'Full Stack Developer',
-        start: '12/17/23',
-        end: 'present',
-        location: 'Cordoba, Argentina',
-        description: 'Full Stack Developer focus on Front End with HTML, CSS, JavaScript and React. Responsive and Mobile Device'
-      }
-    
-    ],
-    education: [
-      {
-        school: 'The Odin Project',
-        degree: 'Full Stack Developer',
-        start: '04/10/23',
-        end: '02/01/24',
-        location: 'Remote',
-        description: 'Full Stack Developer focus on Front End with HTML, CSS, JavaScript and React. Responsive and Mobile Device'
-      },
 
-      {
-        school: 'Helsinsky University',
-        degree: 'Full Stack Open',
-        start: '04/10/23',
-        end: '02/01/24',
-        location: 'Remote',
-        description: 'React, MongoDB, GraphQl, Redux'
-      }
-    ],
-    skills: [
-      'HTML, CSS, JavaScript, TailwindCSS, React'
-    ]
-  }
   const [layout, updateLayout] = useState('resumeTop');
   const [color, updateColor] = useState('#5165F5');
   const [fonts, updateFonts] = useState('sans-serif');
   const [visible, clearResume] = useState(true);
-  const [example, updateExample] = useState(InfoExample);
+ 
 
   
 
@@ -64,17 +23,21 @@ export default function App() {
         updateColor={updateColor} 
         updateFonts={updateFonts} 
         color={color} 
-        example={example} 
-        updateExample={updateExample} 
         clear={visible} 
-        clearResume={clearResume}/>
+        clearResume={clearResume}
+        />
 
-      <Resume 
+      {/* <Resume 
         layout={layout} 
         color={color} 
         fonts={fonts} 
         info={example} 
-        visible={visible}/>
+        visible={visible}
+        fullname={fullname}
+        email={email}
+        phone={phone}
+        address={address}
+        /> */}
     </div>
   )
 }
