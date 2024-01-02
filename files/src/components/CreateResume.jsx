ff2c3bac75c80980854d714b28386d43f4c4842c
Franco Mendez <125481413+francomendezok@users.noreply.github.com>
2024-01-02 20:39:40 -0300

    Trying to fix images sources on Vercel

--- files/src/components/CreateResume.jsx
+++ files/src/components/CreateResume.jsx
@@ -322,35 +322,35 @@ export default function CreateResume({updateLayout, updateColor, updateFonts, co
       <div className="infoSection">
         <div className="buttonContainer flex mb-8 rounded-2xl">
             <Btn
-              img={'../src/assets/file.png'}
+              img={'./src/assets/file.png'}
               alt={'Content'}
               btnClass={'btnContent rounded-l-xl'}
               text={'Personal Info'}
               callback={Content}
             />
             <Btn
-              img={'../src/assets/edit.png'}
+              img={'./src/assets/edit.png'}
               alt={'Edit'}
               btnClass={'btnCustomize'}
               text={'Customize Layout'}
               callback={Layout}
             />
             <Btn
-              img={'../src/assets/clear.png'}
+              img={'./src/assets/clear.png'}
               alt={'Clear'}
               btnClass={'btnClear'}
               text={'Clear Resume'}
               callback={hideResume}
             />
             <Btn
-              img={'../src/assets/example.png'}
+              img={'./src/assets/example.png'}
               alt={'Example'}
               btnClass={'btnExample'}
               text={'Load Example'}
               callback={showResume}
             />
             <Btn
-              img={'../src/assets/pdf.png'}
+              img={'./src/assets/pdf.png'}
               alt={'PDF Downloader'}
               btnClass={'btnPDF rounded-e-xl'}
               text={'Download PDF'}
